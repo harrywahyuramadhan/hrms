93c6179c5301cf34930a71922c0b4d628f709c58
harrywahyuramadhan <rumahitalpa@gmail.com>
2025-05-05 23:42:56 +0700

    test

--- docker/init.sh
+++ docker/init.sh
@@ -1,42 +1,46 @@
-#!bin/bash
+#!/bin/bash
+
+set -e
 
 if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
     echo "Bench already exists, skipping init"
     cd frappe-bench
+    bench setup requirements
     bench start
-else
-    echo "Creating new bench..."
+    exit 0
 fi
 
-export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
-
-bench init --skip-redis-config-generation frappe-bench
-
+echo "Creating new bench..."
+bench init --frappe-branch version-15 --skip-redis-config-generation --python python3 frappe-bench
 cd frappe-bench
 
-# Use containers instead of localhost
-bench set-mariadb-host mariadb
-bench set-redis-cache-host redis:6379
-bench set-redis-queue-host redis:6379
-bench set-redis-socketio-host redis:6379
+bench set-config -g redis_cache redis://redis:6379
+bench set-config -g redis_queue redis://redis:6379
+bench set-config -g redis_socketio redis://redis:6379
+bench set-config -g db_host mariadb
 
-# Remove redis, watch from Procfile
-sed -i '/redis/d' ./Procfile
-sed -i '/watch/d' ./Procfile
+bench get-app erpnext --branch version-15
+bench get-app hrms --branch version-15
 
-bench get-app erpnext
-bench get-app hrms
+echo "Waiting for MariaDB..."
+until mysqladmin ping -h mariadb --silent; do
+  sleep 1
+done
 
 bench new-site hrms.localhost \
---force \
---mariadb-root-password 123 \
---admin-password admin \
---no-mariadb-socket
+  --mariadb-root-password 123 \
+  --admin-password admin \
+  --no-mariadb-socket
 
+bench --site hrms.localhost install-app erpnext
 bench --site hrms.localhost install-app hrms
+
 bench --site hrms.localhost set-config developer_mode 1
 bench --site hrms.localhost enable-scheduler
 bench --site hrms.localhost clear-cache
 bench use hrms.localhost
+
+yarn install --cwd apps/frappe
+bench setup requirements
 
 bench start
